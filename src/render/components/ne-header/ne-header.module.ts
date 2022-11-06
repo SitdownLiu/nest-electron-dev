d3e22ba9e2bf9359086bea8bd32ef0cf9d352af4
sitdown.liu <sitdown.liu@gmail.com>
2022-11-07 00:14:20 +0800

    v20221107 -Tools: 主题设置

--- src/render/components/ne-header/ne-header.module.ts
+++ src/render/components/ne-header/ne-header.module.ts
@@ -1,12 +1,18 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NeHeaderComponent } from './ne-header.component';
 import { TabsModule } from 'ng-devui/tabs';
 import { IconModule } from 'ng-devui/icon';
+import { NeToolsThemeComponent } from './ne-tools-theme/ne-tools-theme.component';
+import { PopoverModule } from 'ng-devui/popover';
+import { ButtonModule } from 'ng-devui/button';
+import { TooltipModule } from 'ng-devui/tooltip';
+import { RadioModule } from 'ng-devui';
 
 @NgModule({
-  declarations: [NeHeaderComponent],
-  imports: [CommonModule, TabsModule, IconModule],
+  declarations: [NeHeaderComponent, NeToolsThemeComponent],
+  imports: [CommonModule, TabsModule, IconModule, ButtonModule, PopoverModule, TooltipModule, RadioModule, FormsModule],
   exports: [NeHeaderComponent],
 })
 export class NeHeaderModule {}
---
+++ src/render/components/ne-header/ne-tools-theme/ne-tools-theme.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ThemeServiceInit, devuiDarkTheme, Theme, ThemeService } from 'ng-devui/theme';
+import { infinityTheme, sweetTheme, provenceTheme, deepTheme, galaxyTheme } from 'ng-devui/theme-collection';
+
+@Component({
+  selector: 'app-ne-tools-theme',
+  templateUrl: './ne-tools-theme.component.html',
+  styleUrls: ['./ne-tools-theme.component.scss'],
+})
+export class NeToolsThemeComponent implements OnInit {
+  themeList: any = { infinityTheme, sweetTheme, provenceTheme, deepTheme, devuiDarkTheme, galaxyTheme };
+  themeService: ThemeService;
+
+  activeTheme: string = 'infinityTheme';
+  themeOptions = [
+    { name: '无限主题', value: 'infinityTheme' },
+    { name: '蜜糖主题', value: 'sweetTheme' },
+    { name: '紫罗兰主题', value: 'provenceTheme' },
+    { name: '深邃夜空主题', value: 'deepTheme' },
+    { name: '追光主题', value: 'galaxyTheme' },
+    { name: '暗黑主题', value: 'devuiDarkTheme' },
+  ];
+  popoverStyle = { backgroundColor: '', width: '160px' };
+
+  constructor() {
+    this.themeService = ThemeServiceInit({ ...this.themeList });
+  }
+
+  ngOnInit(): void {
+    this.toggleTheme('infinityTheme');
+  }
+
+  toggleTheme(theme: string) {
+    this.themeService.applyTheme(this.themeList[theme]);
+    if (['infinityTheme', 'sweetTheme', 'provenceTheme', 'deepTheme'].indexOf(theme) > -1) {
+      this.popoverStyle.backgroundColor = '#ffffff';
+    } else {
+      this.popoverStyle.backgroundColor = '';
+    }
+  }
+}
